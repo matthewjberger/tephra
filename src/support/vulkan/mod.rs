9b7a00a78190ca29db6a94dfa939653829525946
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-05-22 18:09:27 -0600

    Add shader compiler code. Add missing dependencies.

--- src/support/vulkan/mod.rs
+++ src/support/vulkan/mod.rs
@@ -1,9 +1,6 @@
-pub use self::{
-    core::*,
-    resource::*,
-    renderer::*,
-};
+pub use self::{core::*, renderer::*, resource::*, shader_compilation::*};
 
 pub mod core;
 pub mod renderer;
 pub mod resource;
+pub mod shader_compilation;
